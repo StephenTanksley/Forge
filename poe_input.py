988b72f2ad201cc13a7ea5eb49b4345af45c4e45
Stephen Tanksley <stephen.tanksley@gmail.com>
2021-01-29 15:21:04 -0600

    misc: Turned on blocking listener.

--- poe_input.py
+++ poe_input.py
@@ -1,5 +1,6 @@
 from pynput.mouse import Button, Controller
 from pynput import mouse, keyboard
+from sys import exit
 
 
 class Mouse:
@@ -17,32 +18,24 @@ class Mouse:
 
     def on_move(self, x, y):
 
-        self.x = int(round(x))
-        self.y = int(round(y))
-
+        self.x = int(round(x, 2))
+        self.y = int(round(y, 2))
         print('Pointer moved to {0}, {1}'.format(self.x, self.y))
 
     def on_click(self, x, y, button, pressed):
 
-        x = int(round(x))
-        y = int(round(y))
-
+        x = int(round(x, 2))
+        y = int(round(y, 2))
         print('{0} at {1}'.format(
             'Pressed' if pressed else 'Released', (self.x, self.y)))
-        print(button, pressed)
 
     def on_scroll(self, x, y, dx, dy):
 
-        x = int(round(x))
-        y = int(round(y))
+        x = int(round(x, 2))
+        y = int(round(y, 2))
 
         print('Scrolled {0} at {1}'.format(
             'down' if dy < 0 else 'up', (self.x, self.y)))
-
-
-mouse = Mouse()
-print('\n')
-print(mouse, "\n")
 
 
 class Keyboard:
@@ -56,34 +49,40 @@ class Keyboard:
     def __repr__(self):
         return [self.key]
 
-    def on_press(self):
+    def on_press(self, key):
         try:
+            self.key = key
             print('alphanumeric key {0} pressed'.format(self.key.char))
         except AttributeError:
             print('special key {0} pressed'.format(self.key))
 
-    def on_release(self):
-        print('{0} released'.format(
+    def on_release(self, key):
+        print('alphanumeric key {0} released'.format(
             self.key))
-        if self.key == keyboard.key.esc:
+        if self.key == keyboard.Key.esc:
             # Stop listener
-            return False
+            print("Exiting keyboard listener. Goodbye!")
+            exit()
 
 
-keyboard = Keyboard()
-print(keyboard, "\n")
+test_keyboard = Keyboard()
+test_mouse = Mouse()
 
 # BLOCKING - Need to figure out how to get these working inside a MAIN loop using the tkinter GUI.
 
-# with mouse.Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as mouse_listener:
-#     mouse_listener.join()
+with mouse.Listener(on_move=test_mouse.on_move, on_click=test_mouse.on_click, on_scroll=test_mouse.on_scroll) as mouse_listener:
+    mouse_listener.join()
 
-# with keyboard.Listener(on_press=on_press, on_release=on_release) as keyboard_listener:
+# with keyboard.Listener(on_press=test_keyboard.on_press, on_release=test_keyboard.on_release) as keyboard_listener:
 #     keyboard_listener.join()
 
 
-# mouse_listener = mouse.Listener(on_click=on_click, on_press=on_press)
+# NON-BLOCKING - I think this is the implementation I'll need because I can't be blocking the main loop of the GUI.
+
+# mouse_listener = mouse.Listener(
+#     on_move=test_mouse.on_move, on_click=test_mouse.on_click, on_scroll=test_mouse.on_scroll)
 # mouse_listener.start()
 
-# keyboard_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+# keyboard_listener = keyboard.Listener(
+#     on_press=test_keyboard.on_press, on_release=test_keyboard.on_release)
 # keyboard_listener.start()
