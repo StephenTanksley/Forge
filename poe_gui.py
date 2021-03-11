3b76e221a437516ae15889404446703aede3e5a2
Stephen Tanksley <stephen.tanksley@gmail.com>
2021-03-11 08:52:14 -0600

    feat: Adding variables to handle (x, y) tuples.

--- poe_gui.py
+++ poe_gui.py
@@ -9,16 +9,18 @@ class POEForge(teek.Frame):
     def __init__(self, window, *args, **kwargs):
         super().__init__(window, *args, **kwargs)
 
-        description = teek.Text(window)
-        print(description.end)
-        # description.insert()
+        # top_frame = teek.Frame(window)
+        # description_1 = "This is a utility for crafting high stat items in Path of Exile. To begin crafting an item, click on the 'Target (x): ' button, then move to the currency item and left-click. Repeat for the other targets. Set the stat tier and max iterations and then save all preferences in a named preset. Then click the run button. If you need to stop execution at any time during the runtime, press the Stop button."
 
-        # , "This is a utility for crafting high stat items in Path of Exile. \nTo begin crafting an item, click on the 'Target (x): ' button, then move to the currency item and left-click. \nRepeat for the other targets. Set the stat tier and max iterations and then save all preferences in a named preset. Then click the run button. If you need to stop execution at any time during the runtime, press the Stop button."
+        # description = teek.Text(top_frame)
+        # description.insert(description.start, description_1)
+        # description.end
 
-        description.grid(column=0, row=0)
+        # description.grid(column=0, row=0, sticky='nswe')
+        # description.pack(fill='x')
 
-        # Creating a frame for targets.
-        targets = teek.LabelFrame(window, text="Targets")
+        """LABELS"""
+        targets = teek.Frame(window)
         targets.grid(column=1, row=1)
 
         target1 = teek.Label(targets, text="Target 1: ")
@@ -27,10 +29,35 @@ class POEForge(teek.Frame):
         target2 = teek.Label(targets, text="Target 2: ")
         target2.grid(column=1, row=3)
 
+        item_target = teek.Label(targets, text="Item Target: ")
+        item_target.grid(column=1, row=4)
 
-# test_mouse = Mouse()
+        stat_tier = teek.Label(targets, text="Desired Stat Tier: ")
+        stat_tier.grid(column=1, row=5)
 
-# I'll need a keyboard controller and a mouse controller here eventually.
-# MouseListener = mouse.Listener(
-#     on_move=test_mouse.on_move, on_click=test_mouse.on_click, on_scroll=test_mouse.on_scroll)
-# MouseListener.start()
+        max_iterations = teek.Label(targets, text="Max # of iterations: ")
+        max_iterations.grid(column=1, row=6)
+
+        target_1_limit = teek.Label(targets, "Target 1 currency limit: ")
+        target_1_limit.grid(column=6, row=2)
+
+        target_2_limit = teek.Label(targets, "Target 2 currency limit: ")
+        target_2_limit.grid(column=6, row=3)
+
+        """VARIABLES"""
+
+        """
+        To use variables, you can't just try to manually assign them like you would normally in Python. a = 10 won't work. In the case of a, you'd need to write a.set(new_value) because we're technically using a subclass of TclVariable. We need to use the getter/setter methods to access and modify data stored in each object instance.
+        """
+        t1_x_var = teek.FloatVar()
+        t1_y_var = teek.FloatVar()
+
+        t2_x_var = teek.FloatVar()
+        t2_y_var = teek.FloatVar()
+
+        t1_limit_var = teek.IntVar()
+        t2_limit_var = teek.IntVar()
+
+        """BUTTONS"""
+
+        """INPUT FIELDS"""
