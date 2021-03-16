17250fab5d4658ca55b0a11dee8cf5e7577fd006
Stephen Tanksley <stephen.tanksley@gmail.com>
2021-03-16 17:56:33 -0500

    feat: started planning test framework.

--- poe_tests.py
+++ poe_tests.py
@@ -10,8 +10,17 @@ class InterfaceTest(unittest.TestCase):
 
         window = teek.Window("Testing Window")
         window.geometry(800, 600)
-        gui = POEForge(window)
+        ui = POEForge(window)
 
         mouse = Mouse()
         keyboard = Keyboard()
         preset = Preset("test")
+
+    def preset_testing(self):
+        pass
+
+    def input_testing(self):
+        pass
+
+    def gui_testing(self):
+        pass
