0a8a8e0073a049db272a8853ad9ed71e28c0450d
Stephen Tanksley <stephen.tanksley@gmail.com>
2021-03-24 14:42:23 -0500

    feat: Added event bus to tests.

--- poe_tests.py
+++ poe_tests.py
@@ -1,11 +1,49 @@
 import unittest
-from poe_gui import POEForge
+from poe_ui import POEForge
 from poe_preset import Preset
 from poe_input import Mouse, Keyboard
+from event_bus import EventBus
 import teek
 
+# Unit Tests
+# These will test whether the lights are working.
 
-class InterfaceTest(unittest.TestCase):
+
+class InputUnitTest(unittest.TestCase):
+    def setUp(self):
+        bus = EventBus()
+        mouse = Mouse(bus)
+        keyboard = Keyboard(bus)
+
+
+class PresetUnitTest(unittest.TestCase):
+    def setUp(self):
+        bus = EventBus()
+        mouse = Mouse(bus)
+        keyboard = Keyboard(bus)
+        preset = Preset("test", "test helmet", (0, 1), (1, 2))
+
+        window = teek.Window("Testing Window")
+        window.geometry(800, 600)
+        ui = POEForge(window, bus)
+
+    def test_preset(self):
+        pass
+
+    def test_input(self):
+        pass
+
+    def test_ui(self):
+        pass
+
+
+class UIUnitTest(unittest.TestCase):
+    pass
+    # Integration tests
+    # These will test whether systems are integrated properly and passing data where it needs to go.
+
+
+class IntegrationTests(unittest.TestCase):
     def setUp(self):
 
         window = teek.Window("Testing Window")
@@ -14,14 +52,4 @@ class InterfaceTest(unittest.TestCase):
 
         mouse = Mouse()
         keyboard = Keyboard()
-        preset = Preset("test")
-
-    def test_preset(self):
-
-        pass
-
-    def test_input(self):
-        pass
-
-    def test_ui(self):
-        pass
+        preset = Preset("test", "test helmet", (0, 1), (1, 2))
